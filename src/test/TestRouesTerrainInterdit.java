13e3f6a6b58a23ed1d6f1902bfcb73ab02699783
lauralassance <laura.lassance@gmail.com>
2016-11-15 17:18:22 +0100

    Adding test to verify if robot doesn't go to the cases that it can not go

---
+++ src/test/TestRouesTerrainInterdit.java
@@ -0,0 +1,63 @@
+package test;
+import java.awt.Color;
+import java.io.FileNotFoundException;
+import java.util.zip.DataFormatException;
+
+import donnee.Direction;
+import gui.GUISimulator;
+import io.Simulateur;
+import robot.Robot;
+import evenement.*;
+
+public class TestRouesTerrainInterdit {
+
+	private final static int tailleSimulateur = 800;
+	
+	public static void main(String[] args) {
+		if (args.length < 1) {
+            System.out.println("Syntaxe: java TestLecteurDonnees <nomDeFichier>");
+            System.exit(1);
+        }
+
+        try {
+            GUISimulator gui = new GUISimulator(TestRouesTerrainInterdit.tailleSimulateur, 
+            								    TestRouesTerrainInterdit.tailleSimulateur, 
+            									Color.BLACK);
+            
+            Simulateur simulation = new Simulateur(gui,
+            									   args[0],
+            									   TestRouesTerrainInterdit.tailleSimulateur,             									  
+            									   TestRouesTerrainInterdit.tailleSimulateur);
+            
+            createScenarie(simulation);
+            
+            
+            
+        } catch (FileNotFoundException e) {
+            System.out.println("fichier " + args[0] + " inconnu ou illisible");
+        } catch (DataFormatException e) {
+            System.out.println("\n\t**format du fichier " + args[0] + " invalide: " + e.getMessage());
+        }
+
+	}
+	
+	private static void createScenarie(Simulateur simulation) {
+		
+		Robot robot = simulation.getDonnees().getRobots().get(0);
+		int i=1;
+		simulation.ajouteEvenement( new DeplacerEvenement(i++, 
+				  robot,
+				  Direction.EST, 
+				  simulation.getDonnees().getCarte()) );
+		simulation.ajouteEvenement( new DeplacerEvenement(i++, 
+				  robot,
+				  Direction.SUD, 
+				  simulation.getDonnees().getCarte()) );
+		for (int j=0; j< 6; j++)
+			simulation.ajouteEvenement( new DeplacerEvenement(i+j, 
+															  robot,
+															  Direction.NORD, 
+															  simulation.getDonnees().getCarte()) );
+	}
+
+}
